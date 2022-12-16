7bab2eb6874e503d07b96ea67f91f1c184db17f6
Perri Love <perrialexislove@gmail.com>
2022-12-16 09:11:57 -0800

    add Download XLSX button for PAD research

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CV from '../../assets/FSE-Dec-2021.docx';
+import TestDocument from '../../assets/TestFile.xlsx';
 
 const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's talk!</a>
+        <a href={TestDocument} className="btn">Download XLSX</a>
     </div>
   )
 }
