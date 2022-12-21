e0c8fd3e053f1687be37929bd468b190e339a8be
Perri Love <perrialexislove@gmail.com>
2022-12-21 11:21:55 -0800

    update picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/poet_crop-nobg.png'
+import ME from '../../assets/morocco_dup_nobg.png';
 import Socials from './Socials';
 import Chatbot from './Chatbot';
 
